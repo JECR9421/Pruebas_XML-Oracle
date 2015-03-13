999e3c9a34bc2729588e5dc247f873022299b8e4
JECR9421 <ch2jsr@gmail.com>
2015-03-13 17:58:37 -0600

    Sección de Extract datos con y sin secuencias

--- conexion_local.sql
+++ conexion_local.sql
@@ -24,6 +24,11 @@ select XMLELEMENT("Emp",
    XMLFOREST(e.FIRST_NAME, e.PHONE_NUMBER, e.salary,(select count(*)from HR.EMPLOYEES)as total))
 FROM HR.EMPLOYEES e;
 
+---XMLGEN es similar a XMLELEMENT o inclusive XMLFORREST solo que devuelve un doc completo con la estructura xml y no solo un conjunto de tags
+SELECT SYS_XMLGen(XMLELEMENT("Emp", 
+   XMLFOREST(e.FIRST_NAME, e.PHONE_NUMBER, e.salary)))
+FROM HR.EMPLOYEES e;
+
 --XMLAGG imprime varias columnas agrupadas en una solo fila se utiliza para manejar sentencias del tipo de 'order by'
 
 SELECT XMLELEMENT("Empleados",
@@ -79,6 +84,76 @@ SELECT XMLParse(CONTENT
   AS po FROM DUAL d;
 
 ---Extracción de datos---------------------------------------------------------------
+/*SELECT EXTRACT(warehouse_spec, '/Warehouse') as "Warehouse"
+   FROM warehouses WHERE warehouse_name = 'San Francisco';*/
+--Extrat saca los nodos que le son indicados  de la forma extract (xml_file,'nodos que quiero sacar //nodo_padre/nodo_hijo')
+SELECT
+extract(
+xmltype('<DATA>
+  <ROWS etapa="2"     hilo_activo="14804"    hilo="6325"    cerrar="S"  usuarioelegido="0"  plantilla="8">
+    <ROW  secuencia="18351">ESTO PRUEBA</ROW>
+  </ROWS>
+</DATA>'),'//ROWS/ROW') hilo
+   FROM dual;   
+   
+-- En caso de existir más de un elemento dentro del mismo nodo o tag es necesario especificar cual se desea sacar 
+--Como si fuera un arreglo [#] # --> iniciando en 1 del modo //nodo_padre/nodo[#]
+
+SELECT
+extract(
+xmltype('<DATA>
+  <ROWS etapa="2"     hilo_activo="14804"    hilo="6325"    cerrar="S"  usuarioelegido="0"  plantilla="8">
+    <ROW  secuencia="18351">ESTO PRUEBA</ROW>
+    <ROW  secuencia="18352"/>
+  </ROWS>
+</DATA>'),'//ROWS/ROW[2]') hilo
+   FROM dual;   
+   
+--ExtractValue esta función es más avanzada ya que extrae el valor en medio de los <tag> y </tag> en implementación es igual a extract
+
+SELECT
+extractvalue(
+xmltype('<DATA>
+  <ROWS etapa="2"     hilo_activo="14804"    hilo="6325"    cerrar="S"  usuarioelegido="0"  plantilla="8">
+    <ROW  secuencia="18351">ESTO PRUEBA</ROW>
+    <ROW  secuencia="18352"/>
+  </ROWS>
+</DATA>'),'//ROWS/ROW[1]') hilo
+   FROM dual;   
+  
+--Para extraer múltiples valores es necesario utilizar XMLSEQUENCE que genera algo similar a un array de con los elementos contenidos en 
+--el /nodo especificado al generar un varray necesita utilizar la sentencia from
+--extract value no soporta multiples valores 
+--extract por su parte si pero los imprime como una fila unica
+--Por estos motivos es recomendable utilizar XMLSequence para el manejo de múltiples datos dentro de un nodo o tag
+
+SELECT value(T).getstringval() Attribute_Value
+  FROM table(XMLSequence(extract(XMLType('<A><B>V1</B><B>V2</B><B>V3</B></A>'),
+                                 '/A/B'))) T;
+                                 
+--Aplicado al ejemplo anterior
+--Comparando ejecución de extract con secuencia y sin la misma
+SELECT value(T).getstringval() Attribute_Value
+  FROM table(XMLSequence(extract(
+xmltype('<DATA>
+  <ROWS etapa="2"     hilo_activo="14804"    hilo="6325"    cerrar="S"  usuarioelegido="0"  plantilla="8">
+    <ROW  secuencia="18351">ESTO PRUEBA</ROW>
+    <ROW  secuencia="18352"/>
+  </ROWS>
+</DATA>'),'//ROWS/ROW'))) T;
+
+SELECT
+extract(
+xmltype('<DATA>
+  <ROWS etapa="2"     hilo_activo="14804"    hilo="6325"    cerrar="S"  usuarioelegido="0"  plantilla="8">
+    <ROW  secuencia="18351">ESTO PRUEBA</ROW>
+    <ROW  secuencia="18352"/>
+  </ROWS>
+</DATA>'),'//ROWS/ROW') hilo
+   FROM dual; 
+   
+
+
 
 
 
