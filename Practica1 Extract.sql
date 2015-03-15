4ee1f743a1af007e2eefd8196acebffe8a2a7d4e
JECR9421 <ch2jsr@gmail.com>
2015-03-15 15:38:37 -0600

    Extraccion valor con for

--- Practica1 Extract.sql
+++ Practica1 Extract.sql
@@ -3,6 +3,7 @@ declare
    cant_nodos NUMBER;
    xml Clob;
    valor VARCHAR2(125);
+   tag varchar2(3);
  begin
  cant_nodos := 0;
  xml:= '<SdtRequest>
@@ -22,19 +23,59 @@ declare
   </Parametros>
   </SdtRequest>';
   
-  GetRegistrosXML(xml,'//SdtRequest/Header',cant_nodos);
+  GetRegistrosXML(xml,'//SdtRequest/Header/*',cant_nodos);
    DBMS_OUTPUT.PUT_LINE (cant_nodos);
-  FOR i IN 1..cant_nodos LOOP
-    SELECT into valor
-extractvalue(
-/*xmltype(xml),'//SdtRequest/Header'||'['||i||']') hilo*/
-xmltype(xml),'//SdtRequest/Header/Object_Id') hilo
-   FROM dual;
+   
+   for i in 1..cant_nodos loop 
+   tag := to_char(i);
+   SELECT
+    extractvalue(xml,'//SdtRequest/Header/*['|| tag ||']') columna into valor
+       FROM dual;   
    DBMS_OUTPUT.PUT_LINE (valor);
-  END LOOP;
+   end loop;
+  
 end;
 
-SELECT
+  
+  SELECT value(T).getstringval() Attribute_Value
+  FROM table(XMLSequence(extract(xmltype('<SdtRequest>
+  <Header>
+    <Object_Id>299</Object_Id>
+    <Credencial>150808SX8088</Credencial>
+    <UserName>DBPMS</UserName>
+    <SupervisorName>DBPMS</SupervisorName>
+    <Action>E</Action>
+    <Value>1</Value>
+  </Header>
+  <Parametros>
+    <SdtRequest.Parametro>
+      <P>GV_XML</P>
+      <X><![CDATA[<DATA><ROWS etapa="18"   hilo_activo="14941"     hilo="6328"    cerrar="S"  usuarioelegido="0"  plantilla="11"> </ROWS></DATA>]]></X>
+    </SdtRequest.Parametro>
+  </Parametros>
+  </SdtRequest>'),'//SdtRequest/Header/*'))) T;
+  
+  
+  SELECT extractvalue(value(T), '/Header/Object_Id') "objetc_id"
+  FROM table(XMLSequence(extract(xmltype('<SdtRequest>
+  <Header>
+    <Object_Id>299</Object_Id>
+    <Credencial>150808SX8088</Credencial>
+    <UserName>DBPMS</UserName>
+    <SupervisorName>DBPMS</SupervisorName>
+    <Action>E</Action>
+    <Value>1</Value>
+  </Header>
+  <Parametros>
+    <SdtRequest.Parametro>
+      <P>GV_XML</P>
+      <X><![CDATA[<DATA><ROWS etapa="18"   hilo_activo="14941"     hilo="6328"    cerrar="S"  usuarioelegido="0"  plantilla="11"> </ROWS></DATA>]]></X>
+    </SdtRequest.Parametro>
+  </Parametros>
+  </SdtRequest>'),'//SdtRequest/Header'))) T;
+  
+  
+  SELECT
 extractvalue(
 xmltype('<SdtRequest>
   <Header>
@@ -51,8 +92,8 @@ xmltype('<SdtRequest>
       <X><![CDATA[<DATA><ROWS etapa="18"   hilo_activo="14941"     hilo="6328"    cerrar="S"  usuarioelegido="0"  plantilla="11"> </ROWS></DATA>]]></X>
     </SdtRequest.Parametro>
   </Parametros>
-  </SdtRequest>'),'//SdtRequest/Header/Object_Id') hilo
-   FROM dual;
+  </SdtRequest>'),'//SdtRequest/Header/*[2]') hilo
+   FROM dual;   
 
 
 
